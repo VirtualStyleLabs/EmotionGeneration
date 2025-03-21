6df30596671580091dfed9eb1f87ef1572e955b9
TVNLCHARAN <vnlcharan.t@gmail.com>
2025-03-21 09:44:28 +0530

    create utils in solver

---
+++ solver.py
@@ -0,0 +1,77 @@
+from model import Generator
+from model import Discriminator
+from torchvision.utils import save_image
+import torch
+
+class Solver(object):
+    """Solver for training and testing StarGAN."""
+
+    def __init__(self , rafd_loader, config):
+        """Initialize configurations."""
+
+        # Data loader.
+        self.rafd_loader = rafd_loader
+
+        # Model configurations.
+        self.c_dim = config.c_dim
+        self.image_size = config.image_size
+        self.g_conv_dim = config.g_conv_dim
+        self.d_conv_dim = config.d_conv_dim
+        self.g_repeat_num = config.g_repeat_num
+        self.d_repeat_num = config.d_repeat_num
+        self.lambda_cls = config.lambda_cls
+        self.lambda_rec = config.lambda_rec
+        self.lambda_gp = config.lambda_gp
+
+        # Training configurations.
+        self.dataset = config.dataset
+        self.batch_size = config.batch_size
+        self.num_iters = config.num_iters
+        self.num_iters_decay = config.num_iters_decay
+        self.g_lr = config.g_lr
+        self.d_lr = config.d_lr
+        self.n_critic = config.n_critic
+        self.beta1 = config.beta1
+        self.beta2 = config.beta2
+        self.resume_iters = config.resume_iters
+
+        # Test configurations.
+        self.test_iters = config.test_iters
+
+        # Miscellaneous.
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        # Directories.
+        self.sample_dir = config.sample_dir
+        self.model_save_dir = config.model_save_dir
+        self.result_dir = config.result_dir
+
+        # Step size.
+        self.log_step = config.log_step
+        self.sample_step = config.sample_step
+        self.model_save_step = config.model_save_step
+        self.lr_update_step = config.lr_update_step
+
+        self.build_model()
+
+    def build_model(self):
+        """Create a generator and a discriminator."""
+        self.G = Generator(self.g_conv_dim, self.c_dim, self.g_repeat_num)
+        self.D = Discriminator(self.image_size, self.d_conv_dim, self.c_dim, self.d_repeat_num) 
+
+        self.g_optimizer = torch.optim.Adam(self.G.parameters(), self.g_lr, [self.beta1, self.beta2])
+        self.d_optimizer = torch.optim.Adam(self.D.parameters(), self.d_lr, [self.beta1, self.beta2])
+        self.print_network(self.G, 'G')
+        self.print_network(self.D, 'D')
+            
+        self.G.to(self.device)
+        self.D.to(self.device)
+
+    def print_network(self, model, name):
+        """Print out the network information."""
+        num_params = 0
+        for p in model.parameters():
+            num_params += p.numel()
+        print(model)
+        print(name)
+        print("The number of parameters: {}".format(num_params))
